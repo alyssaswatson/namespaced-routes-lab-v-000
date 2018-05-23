a883f2b29f28c1ae777ded40bd8143f95c03ff63
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-05-23 23:22:34 +0000

    automatically backed up by learn

--- db/migrate/20180523231848_add_preferences.rb
+++ db/migrate/20180523231848_add_preferences.rb
@@ -3,6 +3,6 @@ class AddPreferences < ActiveRecord::Migration
     add_column :preferences, :artist_sort_order, :string
     add_column :preferences, :song_sort_order, :string
     add_column :preferences, :allow_create_songs, :boolean
-    t.boolean  "allow_create_artists"
+    add_column :preferences, :allow_create_artists, :boolean
   end
 end
