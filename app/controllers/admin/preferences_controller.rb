ffb9937b163a8622c54479e87df35f484fd635bd
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-05-23 23:09:58 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -2,4 +2,8 @@ class Admin::PreferencesController < ApplicationController
   def index
 
   end
+
+  def preferences_params
+    params.require(:admin_preference).permit(:allow_create_songs, :allow_create_artists, :song_sort_order, :artist_sort_order)
+  end
 end
